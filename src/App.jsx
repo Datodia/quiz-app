561e7b408901cf77a8dc34895dd97fcbe933992f
Datodia <dato.diasamidze.02@gmail.com>
2025-02-22 16:48:31 +0400

    add home and quesitons page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react'
 import './App.css'
+import Home from './components/Home.jsx'
+import { data } from './questions.js'
+import Questions from './components/Questions.jsx'
 
 function App() {
+  const [displayState, setDisplayState] = useState('home')
+  const [topic, setTopic] = useState('')
 
   return (
     <>
       <h2>Header</h2>
-      <h1>Quiz App</h1>
+      {displayState === 'home' &&
+        <Home
+          setTopic={setTopic}
+          setDisplayState={setDisplayState} 
+          data={data} 
+        />}
+      {displayState === 'questions' && 
+        <Questions 
+          data={data} 
+          topic={topic} 
+          setDisplayState={setDisplayState} 
+        />}
       <h2>queistion</h2>
     </>
   )
